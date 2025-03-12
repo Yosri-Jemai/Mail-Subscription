ac9642061d813587eca14c3ed624a9f29caa60ad
Yosri Jemai <91740252+Yosri-Jemai@users.noreply.github.com>
2025-03-12 08:01:06 +0100

    reload display

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbypcMNSee-D_Bv9K8N3O8nA1EMMzZHNtcLDdf7_vdJJCRSEkCnpTedsnvwHdQZdbQnh/exec'
 const form = document.forms['submit-to-google-sheet']
+const msq = document.getElementById("msg")
 
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => console.log('Success!', response))
+        .then(response => {
+            msg.innerText = "Thank You For Subscribing!"
+            setTimeout(()=>{
+                msg.innerText = ""
+            },4000)
+            form.reset()
+        })
         .catch(error => console.error('Error!', error.message))
 })
